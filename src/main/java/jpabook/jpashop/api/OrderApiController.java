697989719ba88d7086494bab321860d2529ac07a
wyjax <umjugnki@gmail.com>
2020-01-20 17:56:59 +0900

    주문조회 v4

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +23,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class OrderApiController {
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> orderV1() {
@@ -46,6 +49,33 @@ public class OrderApiController {
                 .collect(Collectors.toList());
         return orderDtos;
     }
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> orderV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> orderDtos = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return orderDtos;
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> orderV3₩₩_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                       @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        List<OrderDto> orderDtos = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return orderDtos;
+    }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> orderV4() {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
 
     // 엔티티를 노출하면 안된다.
     @Data
@@ -81,26 +111,5 @@ public class OrderApiController {
             orderPrice = orderItem.getOrderPrice();
             count = orderItem.getCount();
         }
-    }
-
-    @GetMapping("/api/v3/orders")
-    public List<OrderDto> orderV3() {
-        List<Order> orders = orderRepository.findAllWithItem();
-        List<OrderDto> orderDtos = orders.stream()
-                .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
-
-        return orderDtos;
-    }
-
-    @GetMapping("/api/v3.1/orders")
-    public List<OrderDto> orderV3₩₩_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
-                                       @RequestParam(value = "limit", defaultValue = "100") int limit) {
-        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
-        List<OrderDto> orderDtos = orders.stream()
-                .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
-
-        return orderDtos;
     }
 }
